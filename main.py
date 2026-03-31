09b18c31d3090309033e18770d046aec2d0db30e
Lorenzogamesofc <lorenzo109762@gmail.com>
2026-03-31 20:48:40 -0300

    Mudança do code

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-from flask import Flask, render_template
+from FakePinterest import app
 
-app = Flask(__name__)
-
-@app.route("/")
-def homepage():
-    return render_template("homepage.html")
-
-@app.route("/perfil")
-def perfil():
-    return "Perfil do Usuário"
 
 if __name__ == "__main__":
     app.run(debug=True)
