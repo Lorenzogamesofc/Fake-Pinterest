230492d53a929bade527afdd79a3996ac009c8c0
Lorenzogamesofc <lorenzo109762@gmail.com>
2026-03-30 23:28:20 -0300

    Arrumando coisas

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "FakePinterest - Meu primeiro site no "
+    return render_template("homepage.html")
+
+@app.route("/perfil")
+def perfil():
+    return "Perfil do Usuário"
 
 if __name__ == "__main__":
     app.run(debug=True)
