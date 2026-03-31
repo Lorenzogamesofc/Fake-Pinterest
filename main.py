cdcf5554523b83db2e4383a3f1cc200878e1404f
Lorenzogamesofc <lorenzo109762@gmail.com>
2026-03-30 22:36:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "FakePinterest - Meu quinto site no ar"
+    return "FakePinterest - .:."
 
 if __name__ == "__main__":
     app.run(debug=True)
